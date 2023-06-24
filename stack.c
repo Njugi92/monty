cafd98c2ed220a794d37fab8724aa983910d549c
Njugi92 <kenkaruma.kk@gmail.com>
2023-06-23 19:18:48 -0700

    stack & queue

---
+++ stack.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * f_stack - function prints the top
+ * @head: stack head of stack_t linked list
+ * @counter: line_number
+ * Return: none
+ */
+
+void f_stack(stack_t **head, unsigned int counter)
+{
+(void)head;
+(void)counter;
+bus.lifi = 0;
+}
